45639f3e0e33ac136c309e2710128e2a88cccdf9
Mask <2421697126@qq.com>
2018-10-29 15:29:52 +0800

    新建

---
+++ src/main/java/com/edu/spring/App.java
@@ -0,0 +1,18 @@
+package com.edu.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyBean.class);
+    	System.out.println(context.getBean(MyBean.class));
+    	System.out.println(context.getBean("myBean"));
+    	context.close();
+    }
+}
